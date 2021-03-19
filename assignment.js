b8e586ebaac99a366789818d1bb0fd698e829922
Nekko <nekkostreet@gmail.com>
2021-03-19 10:19:55 -0700

    first commit

---
+++ assignment.js
@@ -0,0 +1,5 @@
+let country = 'USA';
+let continent = 'North America';
+let population = 14442666;
+console.log(country + " " + continent + " " + population);
+let isIsland = country;
---
+++ main.js
@@ -0,0 +1,11 @@
+let js = "amazing";
+if (js === "amazing") {
+    console.log("Welcome");
+};
+let me = {
+    firstName: "Nekko",
+    lastName: "Street",
+    age: 20
+};
+let firstName = "Nekko";
+console.log(me);
